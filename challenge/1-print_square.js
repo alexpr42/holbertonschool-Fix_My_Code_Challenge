7f59dc3e698761540c17485075725159c161c1fe
alexpr42 <8988@holbertonstudents.com>
2024-09-12 21:43:36 -0400

    1-print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,25 +1,29 @@
 #!/usr/bin/node
 /*
-    Prints square with the character
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
 */
 
+
 if (process.argv.length <= 2) {
-    process.stderr.write("Falta el argumento\n");
-    process.stderr.write("Uso: ./1-print_square.js <size>\n");
-    process.stderr.write("Ejemplo: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10);  // Convertir a base 10
+// Parse the argument as a base 10 integer
+const size = parseInt(process.argv[2], 10);
 
+// Validate if the size is a valid number
 if (isNaN(size) || size <= 0) {
-    process.stderr.write("El argumento debe ser un número válido mayor que 0\n");
+    process.stderr.write("Size must be a positive integer\n");
     process.exit(1);
 }
 
+// Print the square
 for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+    console.log("#".repeat(size));
 }
