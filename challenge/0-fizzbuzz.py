06aaf3b735fb693e5f8b5f3b766223a9e108f975
alexpr42 <8988@holbertonstudents.com>
2024-09-12 21:34:34 -0400

    0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -12,7 +12,6 @@ def fizzbuzz(n):
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-
     if n < 1:
         return
 
@@ -21,9 +20,9 @@ def fizzbuzz(n):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            tmp_result.append("fizz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
-            tmp_result.append("buzz")
+            tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
